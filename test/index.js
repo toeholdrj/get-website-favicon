c97da6ba839c39e245541a37504126971fe3d0b1
Brennon Bortz <4292508+toeholdrj@users.noreply.github.com>
2021-01-19 15:44:18 -0800

    Update dependencies

--- test/index.js
+++ test/index.js
@@ -1,7 +1,23 @@
-const getFavicons = require('../')
-const expect = require('chai').expect;
+const getFavicons = require("../");
+const expect = require("chai").expect;
 
-describe('', ()=>{
-    it('', async function(){
-    })
-})
+describe("getFavicons", () => {
+  it("should return a response for a well-known address", (done) => {
+    const url = "https://www.github.com";
+    getFavicons(url).then((result) => {
+      expect(result.icons).to.be.not.empty;
+      done();
+    });
+  }).timeout(10000);
+
+  it("should return an empty list of favicons for a garbage address", (done) => {
+    const url =
+      "https://www.agikjosahgsafdlkjwahejklsahfgjklahfgjhoaeuhjaksfdngaewhrgopasjngklm.com";
+
+    getFavicons(url).then((result) => {
+      expect(result.icons).to.be.an("array");
+      expect(result.icons).to.be.empty;
+      done();
+    });
+  });
+});
